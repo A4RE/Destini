60cd5deda4103c11ec3e95734109b430e4024f3d
Andrey Kovalenko <aakoval01@gmail.com>
2023-10-06 17:21:03 +0300

    add comment

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+    // Comment
     override func viewDidLoad() {
         super.viewDidLoad()
 
